5a004cd8c299a5c8a7c1c5126fbd583c251e25c6
UlrikeWerner <ulrike_werner@web.de>
2023-09-15 13:32:22 +0200

    add repo and create connection

--- src/main/java/com/github/UlrikeWerner/Controller/AsterixController.java
+++ src/main/java/com/github/UlrikeWerner/Controller/AsterixController.java
@@ -1,6 +1,7 @@
 package com.github.UlrikeWerner.Controller;
 
 import com.github.UlrikeWerner.Entities.AsterixCharacter;
+import com.github.UlrikeWerner.Repositories.CharacterRepo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/AsterixAPI/api")
 public class AsterixController {
+    private final CharacterRepo characterRepo;
+
+    public AsterixController(CharacterRepo characterRepo){
+        this.characterRepo = characterRepo;
+    }
 
     @GetMapping("asterixCharacters")
-    public List<AsterixCharacter> findAll(){
-        return List.of( new AsterixCharacter("1", "Asterix", 35, "Krieger"), new AsterixCharacter("2", "Obelix", 35, "Lieferant"), new AsterixCharacter("3", "Miraculix", 60, "Druide"), new AsterixCharacter("4", "Majestix", 60, "Häuptling"), new AsterixCharacter("5", "Troubadix", 25, "Barden"), new AsterixCharacter("6", "Gutemine", 35, "Häuptlingsfrau"), new AsterixCharacter("7", "Idefix", 5, "Hund"), new AsterixCharacter("8", "Geriatrix", 70, "Rentner"), new AsterixCharacter("9", "Automatix", 35, "Schmied"), new AsterixCharacter("10", "Grockelix", 35, "Fischer") );
+    public List<AsterixCharacter> getAsterixCharacterList() {
+        return characterRepo.findAll();
     }
 }
